95b5bbbe326cd9ef182be1f2eb45b4733a9060cf
Murat Şahin <murat.sanih@gmail.com>
2022-02-14 19:02:46 +0300

    Database and DatabaseContext addded, test connnection made

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"active-order-management/domain"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("project start")
+	dbContext := domain.NewDatabaseContext("localhost","active-order-management-db","aomdbuser","aom123.",5432)
+	db,_ := dbContext.Open()
+	dbContext.Close(db)
 }
