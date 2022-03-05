686204b22d81f20d9e0ac27454a63e476e32674a
Murat Şahin <murat.sanih@gmail.com>
2022-03-05 22:13:55 +0300

    Order Line Item formatting fix

--- domain/entity/order_item.go
+++ domain/entity/order_item.go
@@ -1,17 +1,17 @@
 package entity
 
 import (
-	"time"
 	"github.com/shopspring/decimal"
+	"time"
 )
 
 type OrderItem struct {
-	ID string
-	OrderID string
+	ID           string
+	OrderID      string
 	Name         string
 	Description  string
-	Price decimal.Decimal
-	PriceVat decimal.Decimal
+	Price        decimal.Decimal
+	PriceVat     decimal.Decimal
 	IsActive     bool
 	IsDeleted    bool
 	CreatedDate  time.Time
