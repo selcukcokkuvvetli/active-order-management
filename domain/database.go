f2892c711221a4b79d44518df4db87c7afee9e1b
scokkuvvetli <selcuk_cokkuvvetli@hotmail.com>
2022-03-05 19:12:51 +0300

    3. migration added

--- domain/database.go
+++ domain/database.go
@@ -50,6 +50,12 @@ func (dc *DatabaseContext) Migrate(db *sql.DB) error {
 		IsApplied:   false,
 	})
 
+	migrationRepository.Add(entity.Migration{
+		Version:     "0.0.3",
+		Description: "Order  created",
+		IsApplied:   false,
+	})
+
 	migrationInterface, _ := migrationRepository.GetAll()
 	migrations := migrationInterface.([]entity.Migration)
 
