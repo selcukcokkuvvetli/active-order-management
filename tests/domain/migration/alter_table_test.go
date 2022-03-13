f7a1b473a9c10be25b5a5dea90328b59d9347f4e
Murat Şahin <murat.sanih@gmail.com>
2022-03-13 15:59:07 +0300

    commands naming change

--- tests/domain/migration/alter_table_test.go
+++ tests/domain/migration/alter_table_test.go
@@ -11,11 +11,11 @@ import (
 
 func TestAlterTable(t *testing.T) {
 	const tableName = "test_table"
-	const tableColumns = "Alter Column test_id INT"
+	const commands = "Alter Column test_id INT"
 	query := `
 				ALTER TABLE %s
 				  %s;`
-	query = fmt.Sprintf(query, tableName, tableColumns)
+	query = fmt.Sprintf(query, tableName, commands)
 	db,sqlMock,_ := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
 	command := migration.NewCommandContext(*db)
 	defer db.Close()
@@ -23,10 +23,9 @@ func TestAlterTable(t *testing.T) {
 
 	t.Run("Altering table successfully", func(t *testing.T) {
 
-
 		sqlMock.ExpectExec(query).WillReturnResult(sqlmock.NewResult(1, 1))
 
-		err := command.AlterTable(tableName, tableColumns)
+		err := command.AlterTable(tableName, commands)
 
 		assert.Nil(t, err)
 	})
@@ -35,7 +34,7 @@ func TestAlterTable(t *testing.T) {
 		want := errors.New("Altering table failed")
 		sqlMock.ExpectExec(query).WillReturnError(want)
 
-		got := command.AlterTable(tableName, tableColumns)
+		got := command.AlterTable(tableName, commands)
 
 		assert.NotNil(t, got)
 		assert.Equal(t, want, got)
