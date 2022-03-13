9272dc2d5a11a29de4428ce606bf64143bd15e77
Murat Şahin <murat.sanih@gmail.com>
2022-03-13 15:55:04 +0300

    command alter table test

---
+++ tests/domain/migration/alter_table_test.go
@@ -0,0 +1,43 @@
+package migration
+
+import (
+	"active-order-management/domain/migration"
+	"errors"
+	"fmt"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestAlterTable(t *testing.T) {
+	const tableName = "test_table"
+	const tableColumns = "Alter Column test_id INT"
+	query := `
+				ALTER TABLE %s
+				  %s;`
+	query = fmt.Sprintf(query, tableName, tableColumns)
+	db,sqlMock,_ := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	command := migration.NewCommandContext(*db)
+	defer db.Close()
+
+
+	t.Run("Altering table successfully", func(t *testing.T) {
+
+
+		sqlMock.ExpectExec(query).WillReturnResult(sqlmock.NewResult(1, 1))
+
+		err := command.AlterTable(tableName, tableColumns)
+
+		assert.Nil(t, err)
+	})
+
+	t.Run("Alter table returns error", func(t *testing.T) {
+		want := errors.New("Altering table failed")
+		sqlMock.ExpectExec(query).WillReturnError(want)
+
+		got := command.AlterTable(tableName, tableColumns)
+
+		assert.NotNil(t, got)
+		assert.Equal(t, want, got)
+	})
+}
